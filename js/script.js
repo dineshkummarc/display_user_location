224d84a9eaa6adce0c84d83babefb0661ca985f1
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-07-26 16:50:10 +0530

    add

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var latitude;
+var longitude;
+
+function getLocation(){
+	if(navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(a) {
+			latitude = a.coords.latitude;
+			longitude = a.coords.longitude;
+			
+			document.getElementById('latitude').innerHTML = latitude;
+			document.getElementById('longitude').innerHTML = longitude;
+		});
+	}else{
+		alert('Geolocation not supported.');
+	}
+}
